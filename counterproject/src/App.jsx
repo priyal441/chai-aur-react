5e8d64fb24db4a8bc8ee9f29d4ff37a9d7090048
priyal gupta <priyalgupta441@gmail.com>
2025-07-15 22:25:21 +0530

    Added remaining frontend files

--- counterproject/src/App.jsx
+++ counterproject/src/App.jsx
@@ -1,49 +1,42 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [counter, setCounter] = useState(15);
+
+  const [counter, setCounter]  = useState(15)
+
+  //let counter = 15
 
   const addValue = () => {
-    if (counter < 20) {
-      setCounter((prevCounter) => {
-        if (prevCounter < 20) return prevCounter + 1;
-        else return prevCounter;
-      });
-      setCounter((prevCounter) => {
-        if (prevCounter < 20) return prevCounter + 1;
-        else return prevCounter;
-      });
-      setCounter((prevCounter) => {
-        if (prevCounter < 20) return prevCounter + 1;
-        else return prevCounter;
-      });
-      setCounter((prevCounter) => {
-        if (prevCounter < 20) return prevCounter + 1;
-        else return prevCounter;
-      });
-    }
-  };
+    //counter = counter + 1
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1 )
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    
+  }
 
   const removeValue = () => {
-    if (counter > 0) {
-      setCounter(counter - 1);
-    }
-  };
-
+    setCounter(counter - 1)
+  }
+  
   return (
     <>
-      <h1>Chai aur React</h1>
+      <h1>Chai aur react</h1>
       <h2>Counter value: {counter}</h2>
 
-      <button onClick={addValue}>Add value</button>
+      <button
+      onClick={addValue}
+      >Add value {counter}</button> 
       <br />
-      <button onClick={removeValue}>Remove value</button>
-      <p>Footer: {counter}</p>
+      <button
+      onClick={removeValue}
+      >remove value {counter}</button>
+      <p>footer: {counter}</p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
